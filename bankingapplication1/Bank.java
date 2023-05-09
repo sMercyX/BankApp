6ab6cb29fa1245fbe4424894e129879856a2e44a
sMercyX <110713142+sMercyX@users.noreply.github.com>
2023-05-09 12:18:39 +0700

    Add files via upload

---
+++ bankingapplication1/Bank.java
@@ -0,0 +1,18 @@
+
+package bankingapplication1;
+
+
+public class Bank {
+    private String name;
+    
+    
+    public Bank(){
+    }
+    public Bank(String name){
+        this.name = name;
+    }
+    
+    public void openAccount(Account account){
+        
+    }
+}
